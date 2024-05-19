9b9d031e1431067a7d892611e829f65355232460
Bartek Pacyna <b.pacyna@op.pl>
2024-05-20 01:00:22 +0200

    Update usb.js

--- public/JS/Manager/USB.js
+++ public/JS/Manager/USB.js
@@ -1,23 +1,19 @@
 const socket = new WebSocket('ws://localhost:8080');
 
-// Obsługa zdarzenia otwarcia połączenia WebSocket
-socket.addEventListener('open', function (event) {
+socket.addEventListener('open', function (event){
     console.log('WebSocket connection established');
 });
 
-// Obsługa zdarzenia otrzymania wiadomości
-socket.addEventListener('message', function (event) {
+socket.addEventListener('message', function (event){
     // Wyświetlanie otrzymanych danych na stronie
     const dataDisplay = document.getElementById('gpsinfo');
     dataDisplay.innerHTML = 'Received data: ' + event.data;
 });
 
-// Obsługa zdarzenia zamknięcia połączenia WebSocket
-socket.addEventListener('close', function (event) {
+socket.addEventListener('close', function (event){
     console.log('WebSocket connection closed');
 });
 
-// Obsługa zdarzenia błędu połączenia WebSocket
-socket.addEventListener('error', function (event) {
+socket.addEventListener('error', function (event){
     console.error('WebSocket error:', event);
 });
